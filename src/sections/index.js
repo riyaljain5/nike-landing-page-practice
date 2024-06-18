9bc760059f2aaf1a29e9ec18e2e53112dc85f0f8
riyal <riyaljain533@gmail.com>
2024-06-18 23:19:06 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Servies";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+
+
+export{
+    Hero,
+    CustomerReviews,
+    PopularProducts,
+    SpecialOffers,
+    Services,
+    Footer,
+    SuperQuality,
+    Subscribe
+}
